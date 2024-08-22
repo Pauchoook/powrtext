8ccb9a00cdbaac5830aa79ba09dd94cd3c671476
Pauchoook <nkolatarasov@mail.ru>
2024-08-22 19:58:25 +0300

    dropdown

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ import * as functions from "./files/functions.js";
 
 import "../scss/style.scss";
 import spoller from "./files/spoller.js";
+import dropdown from "./files/dropdown.js";
 
 functions.isWebp();
 spoller();
+dropdown();
